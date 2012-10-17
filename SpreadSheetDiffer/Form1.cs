1ff36bfe59565afb99354c05a9360c3bc6fc5d3e
Mark Zachacz <markz5@live.com>
2012-10-16 23:37:59 -0700

    Added some comments.

--- SpreadSheetDiffer/Form1.cs
+++ SpreadSheetDiffer/Form1.cs
@@ -18,14 +18,18 @@ namespace SpreadSheetDiffer
         // Name of the CSV file to create.
         private String outFile = null;
 
+        // Refrence to the instance of Microsoft Excel being used.
         private Excel.Application mExcel = null;
 
+        // Refrences to the first workbook to be diffed.
         private Excel._Workbook mBook1 = null;
         private Excel.Sheets mBook1Sheets = null;
 
+        // Refrences to the second workbook to be diffed.
         private Excel._Workbook mBook2 = null;
         private Excel.Sheets mBook2Sheets = null;
 
+        // Constructor.
         public Form1()
         {
             InitializeComponent();
@@ -40,30 +44,39 @@ namespace SpreadSheetDiffer
 
         }
 
+        // Event handler for the top browse button.
         private void mBook1Load_Click(object sender, EventArgs e)
         {
+            // Create a new OpenFileDialog object that will be used to select the first
+            // workbook.
             System.Windows.Forms.OpenFileDialog fWin = new OpenFileDialog();
 
+            // Set the extensions and default folder path to be used by the dialog.
             fWin.DefaultExt = "xlsx";
             fWin.Filter = "Excel Spreadsheets (*.xlsx)|*.xlsx";
             fWin.InitialDirectory = 
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+            // Open the dialog and do addional processing if a valid result is returned.
             if (fWin.ShowDialog() == DialogResult.OK)
             {
+                // If a workbook is already open close it.
                 mBook1Sheets = null;
                 if(mBook1 != null) {
                     mBook1.Close(false);
                     mBook1 = null;
                 }
 
+                // Open the workbook selected in the file dialog.
                 mBook1File.Text = fWin.FileName;
                 mBook1 = (Excel._Workbook)(mExcel.Workbooks.Add(fWin.FileName));
                 mBook1Sheets = (Excel.Sheets)(mBook1.Sheets);
 
+                // Enable and clear the combobox for the first workbook's worksheets.
                 mBook1Sheet.Enabled = true;
                 mBook1Sheet.Items.Clear();
 
+                // Populate the first workbook's combobox.
                 int numSheets = mBook1Sheets.Count;
                 for(int i = 1; i <= numSheets; i++) {
                     mBook1Sheet.Items.Add(((Excel._Worksheet)(mBook1Sheets.Item[i])).Name);
@@ -71,17 +84,23 @@ namespace SpreadSheetDiffer
             }
         }
 
+        // Event Handler for the middle browse button.
         private void mBook2Load_Click(object sender, EventArgs e)
         {
+            // Create a new OpenFileDialog object that will be used to select the second
+            // workbook.
             System.Windows.Forms.OpenFileDialog fWin = new OpenFileDialog();
 
+            // Set the extensions and default folder path to be used by the dialog.
             fWin.DefaultExt = "xlsx";
             fWin.Filter = "Excel Spreadsheets (*.xlsx)|*.xlsx";
             fWin.InitialDirectory =
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+            // Open the dialog and do addional processing if a valid result is returned.
             if (fWin.ShowDialog() == DialogResult.OK)
             {
+                // If a workbook is currently open close it.
                 mBook2Sheets = null;
                 if (mBook2 != null)
                 {
@@ -89,13 +108,16 @@ namespace SpreadSheetDiffer
                     mBook2 = null;
                 }
 
+                // Open the workbook selected in the dialog.
                 mBook2File.Text = fWin.FileName;
                 mBook2 = (Excel._Workbook)(mExcel.Workbooks.Add(fWin.FileName));
                 mBook2Sheets = (Excel.Sheets)(mBook2.Sheets);
 
+                // Enable and clear the combobox for the second workbook's worksheets.
                 mBook2Sheet.Enabled = true;
                 mBook2Sheet.Items.Clear();
 
+                // Populate the combobox for the second workbook.
                 int numSheets = mBook2Sheets.Count;
                 for (int i = 1; i <= numSheets; i++)
                 {
@@ -104,15 +126,20 @@ namespace SpreadSheetDiffer
             }
         }
 
+        // Event handler for the bottom browse button.
         private void mCreate_Click(object sender, EventArgs e)
         {
+            // Create a new SaveFileDialog object.
             System.Windows.Forms.SaveFileDialog fWin = new SaveFileDialog();
 
-            fWin.DefaultExt = "xlsx";
+            // Set the extensions and default folder path to be used by the dialog.
+            fWin.DefaultExt = "csv";
             fWin.Filter = "CSV Files (*.csv)|*.csv";
             fWin.InitialDirectory =
                 Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+            // Set variables based on the result of the dialog. If an invalid result is
+            // returned do nothing.
             if (fWin.ShowDialog() == DialogResult.OK)
             {
                 outFile = fWin.FileName;
